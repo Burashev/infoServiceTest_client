710a823d24a21d12c48a14f7967634d83da8f1af
Burashev Shamil <shburashev@ya.ru>
2022-04-24 15:20:28 +0300

    add: verify check

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,40 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
+import store from '@/store'
 
 const routes = [
-  {
-    path: '/',
-    name: 'index',
-    component: () => import(/* webpackChunkName: "signIn" */ '../views/SignIn.vue')
-  },
-  {
-    path: '/sign_up',
-    name: 'signUp',
-    component: () => import(/* webpackChunkName: "signIn" */ '../views/SignUp.vue')
-  },
-  // {
-  //   path: '/about',
-  //   name: 'about',
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  // }
+    {
+        path: '/',
+        name: 'index',
+        component: () => import(/* webpackChunkName: "index" */ '../views/Index.vue'),
+        meta: {
+            isVerified: true,
+        }
+    },
+    {
+        path: '/sign_in',
+        name: 'signIn',
+        component: () => import(/* webpackChunkName: "signIn" */ '../views/SignIn.vue')
+    },
+    {
+        path: '/sign_up',
+        name: 'signUp',
+        component: () => import(/* webpackChunkName: "signUp" */ '../views/SignUp.vue')
+    },
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
+})
+
+router.beforeEach((to, from) => {
+    if (to.meta.isVerified && !store.getters["user/isVerified"]) {
+        store.dispatch('notification/createNotification', {
+            text: 'You need verified your email',
+            error: true
+        }).then();
+        return {name: 'signIn'}
+    }
 })
 
 export default router
