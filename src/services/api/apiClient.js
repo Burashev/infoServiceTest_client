d8b46e1e71688bf46f1fe2111fdb6bc1e262f804
Burashev Shamil <shburashev@ya.ru>
2022-04-24 14:54:12 +0300

    add: show notification on error request

--- src/services/api/apiClient.js
+++ src/services/api/apiClient.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import store from '@/store';
 
 const apiClient = axios.create({
     baseURL: 'http://localhost:8000/api',
@@ -7,6 +8,23 @@ const apiClient = axios.create({
         'Accept': 'application/json',
         'Content-Type': 'application/json'
     }
+})
+
+apiClient.interceptors.response.use((response) => {
+    return response.data
+}, (error) => {
+    if (error.response) {
+        const errorMessage = error.response.data.message;
+        store.dispatch('notification/createNotification', {
+            text: errorMessage,
+            error: true
+        }).then();
+    } else store.dispatch('notification/createNotification', {
+        text: error,
+        error: true
+    }).then();
+
+    return Promise.reject(error)
 })
 
 export {apiClient};
